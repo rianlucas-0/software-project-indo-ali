f38034d44a0317732d0c580b5fd6c39d434996a1
Rian Lucas <rianlucas.moon@gmail.com>
2025-07-30 14:43:06 -0300

    refactor(layout): remove unused application-logo component

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,11 +7,11 @@
                 <div class="shrink-0 flex items-center">
                 @if(Auth::check() && Auth::user()->usertype=='admin')
                     <a href="{{ route('admin.dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        
                     </a>
                 @else
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        
                     </a>
                 @endif
                 </div>
